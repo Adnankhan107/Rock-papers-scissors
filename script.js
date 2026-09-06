c2c240bdae30f6c2851079a30abac4d2d29f2c2d
Adnan khan <adnan07khanak@gmail.com>
2026-09-06 15:26:18 +0530

    added comp logic

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
-
 function getComputerChoice(){
-    
+    let compChoice = ["rock", "paper", "scissors"];
+    let randomChoice = Math.floor(Math.random() * compChoice.length) + 1
+
+    if(randomChoice === 1){
+        return "rock";
+    } if(randomChoice === 2){
+        return "paper";
+    } if(randomChoice === 3){
+        return "scissors"
+    }
 }
 
-getComputerChoice();
+console.log(getComputerChoice());
