36f8d6e11390a4031898ce212dfae62cc254e691
Adnan khan <adnan07khanak@gmail.com>
2026-09-06 16:58:36 +0530

    implemented play round feature

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let humanChoice = window.prompt("What's your choice?");
+let humanScore = 0;
+let compScore = 0;
+
 
 function getComputerChoice(){
     let compChoice = ["rock", "paper", "scissors"];
@@ -12,4 +16,39 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice());
+
+
+
+
+function playRound(getComputerChoice, humanChoice){
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === "rock") {  
+    if (getComputerChoice === "rock") {
+      alert( "It is a tie!!");
+    } else if (getComputerChoice === "paper") {
+        alert ("The computer wins!");
+      } else {
+        alert ("You win this game!");
+      }
+  }
+  else if (humanChoice === "paper") {  
+    if (getComputerChoice === "paper") {
+      alert ("It is a tie!!");
+    } else if (getComputerChoice === "rock") {
+        alert ("The computer wins!");
+      } else {
+        alert ("You win this game!");
+      }
+  } else {
+    if (getComputerChoice === "rock") {
+      alert ("Computer wins!");
+  } else if (getComputerChoice === "paper") {
+      alert ("You win!");
+  } else {
+      alert ("Draw!");
+  }
+  }
+
+}
+
+playRound(getComputerChoice(), humanChoice);
